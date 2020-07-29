e473526961160ff19ae9cdb8a2caad29b01ce86d
NickoValentino <nicko.valentinoo@gmail.com>
2020-07-30 04:41:27 +0800

    client setup

---
+++ client/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors:{
+        'black-cvre': '#212121',
+      },
+      fontFamily:{
+        'brand': ['EB Garamond', 'serif']
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
